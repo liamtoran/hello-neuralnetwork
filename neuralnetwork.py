3fa0ea6e5ae37aaafa8cdee58d25e250077cb683
liamkesatoran <bestofthefoo@gmail.com>
2019-05-05 19:59:43 +0200

    added stochastic gradient descent (minibatch) to neuralnetwork.py

--- neuralnetwork.py
+++ neuralnetwork.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random
 
 def sigmoid(x): #sigmoid function of vector x 
 	return 1/(1+np.exp(-x))
@@ -12,7 +13,7 @@ class NeuralNetwork():
 		
 		self.training_inputs = x #training input
 		self.input_size = x.shape[1] #each line of x is an input and each row corresponds to the initial neural activation
-		self.batch_size = x.shape[0] #number of training inputs
+		self.training_batch_size = x.shape[0] #number of training inputs
 		
 		self.training_outputs = y #training_output
 		self.output_size = y.shape[1] #each line of y is the ouput of the input and each row is the corresponding neural activation output. It is important that y is a matrix and never a vector.
@@ -48,14 +49,14 @@ class NeuralNetwork():
 	#Training of the model by adjusting synaptic weight via backpropagation	
 	
 	#Returns gradient of cost function (LSE)
-	def backpropagate(self, z_s, a_s): 
+	def backpropagate(self, z_s, a_s, random_batch): 
 		# y = training output, a_s =  neural activation of front propagation 
 		#
 		dw = [np.zeros(w.shape) for w in self.weights]
 		db = [np.zeros(b.shape) for b in self.biases]
 		deltas = [None] * len(self.weights)
 		#Calculate final error
-		deltas[-1] = (self.training_outputs-a_s[-1])*(sigmoid_derivative(z_s[-1]))
+		deltas[-1] = (self.training_outputs[random_batch]-a_s[-1])*(sigmoid_derivative(z_s[-1]))
 		#Perform BackPropagation
 		#db[-1]=np.ones((self.batch_size,1)).transpose().dot(deltas)/self.batch_size
 		#dw[-1]=np.dot(a_s[-2].transpose(),deltas)
@@ -69,18 +70,26 @@ class NeuralNetwork():
 			
 
 	#Gradient descent
-	def adjust(self, lr = 1):
+	def adjust(self, lr = .1, method ='batch', batch_size=20):
+		if method == 'batch':
 		#Pass training set through neural network
-		z_s, a_s = self.feedforward(self.training_inputs)
-		db, dw = self.backpropagate(z_s,a_s)
+			self.random_batch=range(self.training_batch_size)
+			self.batch_size= self.training_batch_size
+			z_s, a_s = self.feedforward(self.training_inputs)
+			db, dw= self.backpropagate(z_s, a_s, self.random_batch)
+		else :
+			self.batch_size = batch_size
+			self.random_batch = random.sample(range(self.training_batch_size),batch_size)
+			z_s, a_s = self.feedforward(self.training_inputs[self.random_batch])
+			db, dw = self.backpropagate(z_s,a_s,self.random_batch)
 		#Adjusts weights and biases
 		self.weights = [w+lr*dweight for w,dweight in zip(self.weights, dw)]
 		self.biases = [b+lr*dbias for b,dbias in zip(self.biases, db)]
 	
 	#Multiple ajustments
-	def train(self, epochs, lr=1):
+	def train(self, epochs, lr=.1, method='minibatch', batch_size =20):
 		for i in range(epochs):
-			self.adjust(lr)
+			self.adjust(lr,method, batch_size)
 	
 	#Output layer of front propagation
 	def output(self,x):
@@ -88,7 +97,7 @@ class NeuralNetwork():
 	
 	#Returns cost function / error of training inputs compared to training outputs	
 	def training_error(self):
-		np.average((self.training_outputs-self.output(self.training_inputs)**2))
+		return np.average((self.training_outputs-self.output(self.training_inputs))**2)
 	
 	#Draws the neural network
 	def draw(self,coloring = .1):
@@ -106,7 +115,8 @@ if __name__ == "__main__":
 	y = np.array([[0,0],[1,0],[1,1],[0,0],[1,1]])
 	layers = np.array([3,2])
 	nn= NeuralNetwork(x,y,layers)
-	nn.train(10000)
-	print(nn.training_error)
+	nn.train(10000, method='minibatch')
+	print(nn.feedforward(x)[1][-1])
+	#print(nn.training_error())
 	#print(nn.train())
 	#print(nn.errors)
